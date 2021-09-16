074d69c56a4533cda3cab6ad1a939501495a995c
Wooyoung Jang <mhoow6@gmail.com>
2021-09-16 20:25:28 +0900

    Proxy 패턴

---
+++ DesignPattern_Proxy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Proxy 패턴
+// - 대리인 패턴
+// - 가벼운 일은 대리인을 생성해서 맡기고, 무거운 일은 대표가 나와서 해결
+
+namespace DesignPattern_Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Thumbnail> thumbnails = new List<Thumbnail>();
+
+            thumbnails.Add(new ProxyThumbnail("영상 제목 1", "/movie1.mp4"));
+            thumbnails.Add(new ProxyThumbnail("영상 제목 2", "/movie2.mp4"));
+            thumbnails.Add(new ProxyThumbnail("영상 제목 3", "/movie3.mp4"));
+
+            foreach (var thumb in thumbnails)
+            {
+                thumb.ShowTitle();
+            }
+
+            thumbnails[0].ShowPreview();
+        }
+    }
+}
---
+++ DesignPattern_Proxy/Thumbnail.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_Proxy
+{
+    interface Thumbnail
+    {
+        public void ShowTitle();
+        public void ShowPreview();
+    }
+
+    class RealThumbnail : Thumbnail
+    {
+        private string title;
+        private string movieUrl;
+
+        public RealThumbnail(string _title, string _movieUrl)
+        {
+            title = _title;
+            movieUrl = _movieUrl;
+
+            Console.WriteLine(movieUrl + "로부터" + title + "의 영상 데이터 다운");
+        }
+
+        public void ShowPreview()
+        {
+            Console.WriteLine("제목: " + title);
+        }
+
+        public void ShowTitle()
+        {
+            Console.WriteLine(title + "의 프리뷰 재생");
+        }
+    }
+
+    class ProxyThumbnail : Thumbnail
+    {
+        private string title;
+        private string movieUrl;
+        private RealThumbnail realThumbnail;
+
+        public ProxyThumbnail(string _title, string _movieUrl)
+        {
+            title = _title;
+            movieUrl = _movieUrl;
+        }
+
+        public void ShowPreview()
+        {
+            if (realThumbnail == null)
+            {
+                realThumbnail = new RealThumbnail(title, movieUrl);
+            }
+            realThumbnail.ShowPreview();
+        }
+
+        public void ShowTitle()
+        {
+            Console.WriteLine("제목: " + title);
+        }
+    }
+}
