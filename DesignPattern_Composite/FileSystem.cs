1fbab69f2b667ad3fd0d4127da78f01504f951f2
Wooyoung Jang <mhoow6@gmail.com>
2021-09-17 23:48:13 +0900

    컴포지트 패턴

---
+++ DesignPattern_Composite/FileSystem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_Composite
+{
+    interface FileSystem
+    {
+        public int GetSize();
+        public void Remove();
+    }
+
+    class CustomFile : FileSystem
+    {
+        private string name;
+        private int size;
+
+        public CustomFile(string _name, int _size)
+        {
+            name = _name;
+            size = _size;
+        }
+
+        public int GetSize()
+        {
+            Console.WriteLine(name + "파일 크기: " + size);
+            return size;
+        }
+
+        public void Remove()
+        {
+            Console.WriteLine(name + " 파일 삭제");
+        }
+    }
+
+    class CustomFolder : FileSystem
+    {
+        private string name;
+        private List<FileSystem> includeds = new List<FileSystem>();
+
+        public CustomFolder(string _name)
+        {
+            name = _name;
+        }
+
+        public void Add(FileSystem fileSystem)
+        {
+            includeds.Add(fileSystem);
+        }
+
+        public int GetSize()
+        {
+            int total = 0;
+
+            foreach (var included in includeds)
+            {
+                total += included.GetSize();
+            }
+
+            Console.WriteLine(name + "폴더 크기: " + total);
+            Console.WriteLine("------");
+            return total;
+        }
+
+        public void Remove()
+        {
+            foreach (var included in includeds)
+            {
+                included.Remove();
+            }
+
+            Console.WriteLine(name + " 폴더 삭제");
+            Console.WriteLine("----------");
+        }
+    }
+}
---
+++ DesignPattern_Composite/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DesignPattern_Composite
+{
+    // 컴포지트 패턴
+    // - 포함하는 것들과 포함되는 것들이 같은 방식으로 다루도록 하는 패턴
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomFolder schoolFolder = new CustomFolder("학교");
+            CustomFolder grade1Folder = new CustomFolder("1학년");
+            CustomFolder grade2Folder = new CustomFolder("2학년");
+
+            schoolFolder.Add(grade1Folder);
+            schoolFolder.Add(grade2Folder);
+
+            CustomFile enterPhoto = new CustomFile("입학사진", 256);
+            grade1Folder.Add(enterPhoto);
+
+            CustomFolder sem1Folder = new CustomFolder("1학기");
+            CustomFolder sem2Folder = new CustomFolder("2학기");
+
+            grade2Folder.Add(sem1Folder);
+            grade2Folder.Add(sem2Folder);
+
+            CustomFile lecturePlan = new CustomFile("강의계획서", 120);
+            sem1Folder.Add(lecturePlan);
+
+            CustomFolder projFolder = new CustomFolder("프로젝트");
+            sem2Folder.Add(projFolder);
+
+            CustomFile draft = new CustomFile("드래프트", 488);
+            CustomFile finalResult = new CustomFile("결과물", 560);
+
+            projFolder.Add(draft);
+            projFolder.Add(finalResult);
+
+            schoolFolder.GetSize();
+        }
+    }
+}
