c7b5cb95d390c3d89b25ab79855c180bc5198da9
Wooyoung Jang <mhoow6@gmail.com>
2021-09-17 21:57:08 +0900

    추상 팩토리 메소드

---
+++ DesignPattern_AbstractFactoryMethod/Component.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_AbstractFactoryMethod
+{
+    abstract class Component
+    {
+        protected abstract string GetCompName();
+        public Component()
+        {
+            Console.WriteLine(this.GetCompName() + " 생성");
+        }
+    }
+
+    class LightButton : Component
+    {
+        protected override string GetCompName()
+        {
+            return "라이트 버튼";
+        }
+    }
+
+    class DarkButton : Component
+    {
+        protected override string GetCompName()
+        {
+            return "다크 버튼";
+        }
+    }
+
+    class LightSwitch : Component
+    {
+        protected override string GetCompName()
+        {
+            return "라이트 스위치";
+        }
+    }
+
+    class DarkSwitch : Component
+    {
+        protected override string GetCompName()
+        {
+            return "다크 스위치";
+        }
+    }
+
+    class LightDropdown : Component
+    {
+        protected override string GetCompName()
+        {
+            return "라이트 드롭다운";
+        }
+    }
+
+    class DarkDropdown : Component
+    {
+        protected override string GetCompName()
+        {
+            return "다크 드롭다운";
+        }
+    }
+}
---
+++ DesignPattern_AbstractFactoryMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DesignPattern_AbstractFactoryMethod
+{
+    // 추상 팩토리 메소드 패턴
+    // 팩토리 메소드 패턴에서 공장을 abstract이나 interface으로 함으로써 여러 종류로 하는 패턴
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompFactory lightCompFactory = new LightCompFactory();
+            CompFactory darkCompFactory = new DarkCompFactory();
+
+            Component comp1;
+            Component comp2;
+            Component comp3;
+
+            comp1 = lightCompFactory.GetComp(Usage.PRESS); // 라이트 버튼 생성
+            comp2 = lightCompFactory.GetComp(Usage.TOGGLE);
+            comp3 = lightCompFactory.GetComp(Usage.EXPAND);
+
+            comp1 = darkCompFactory.GetComp(Usage.PRESS); // 다크 버튼 생성
+            comp2 = darkCompFactory.GetComp(Usage.TOGGLE);
+            comp3 = darkCompFactory.GetComp(Usage.EXPAND);
+        }
+    }
+}
